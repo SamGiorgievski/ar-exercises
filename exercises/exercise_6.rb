33531cc179f7ef0d118ba4e7f2440959482b6bb7
Samuel Giorgievski <samgiorgievski@gmail.com>
2023-01-23 21:42:47 -0500

    exercise 7

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -14,6 +14,6 @@ puts "----------"
 @store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
 @store1.employees.create(first_name: "Sam", last_name: "G", hourly_rate: 500)
 @store1.employees.create(first_name: "Sponge", last_name: "Bob", hourly_rate: 65)
-@store2.employees.create(first_name: "Patrick", last_name: "Star", hourly_rate: 5)
+@store2.employees.create(first_name: "Patrick", last_name: "Star", hourly_rate: 45)
 @store2.employees.create(first_name: "Leo", last_name: "G", hourly_rate: 80)
 @store2.employees.create(first_name: "Lola", last_name: "G", hourly_rate: 85)
